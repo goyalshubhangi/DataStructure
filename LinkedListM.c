30ee4fec352545e548832b24e9c110d49be34041
goyalshubhangi <36481884+goyalshubhangi@users.noreply.github.com>
2018-08-01 22:53:58 +0530

    Add files via upload

---
+++ LinkedListM.c
@@ -0,0 +1,131 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node *next;
+}*start;
+void createList(int n);
+void insertnode(int data);
+void displayList();
+int main()
+{
+	int data,n;
+	 printf("Enter the total number of nodes: ");
+    scanf("%d", &n);
+    createList(n);
+    printf("\nData in the list \n");
+    displayList();
+    printf("\nEnter data to insert after x in the list: ");
+    scanf("%d", &data);
+    insertnode(data);
+    printf("\nData in the list \n");
+    displayList();
+
+    return 0;
+}
+void createList(int n)
+{
+    struct node *newNode, *temp;
+    int data, i;
+
+    start = (struct node *)malloc(sizeof(struct node));
+
+    if(start == NULL)
+    {
+        printf("Unable to allocate memory.");
+    }
+    else
+    {
+        printf("Enter the data of node 1: ");
+        scanf("%d", &data);
+
+        start->data = data; // Link data field with data
+        start->next = NULL; // Link address field to NULL
+
+        temp = start;
+
+        
+        for(i=2; i<=n; i++)
+        {
+            newNode = (struct node *)malloc(sizeof(struct node));
+            if(newNode == NULL)
+            {
+                printf("Unable to allocate memory.");
+                break;
+            }
+            else
+            {
+                printf("Enter the data of node %d: ", i);
+                scanf("%d", &data);
+
+                newNode->data = data; 
+                newNode->next = NULL;
+                temp->next = newNode; 
+                temp = temp->next; 
+            }
+        }
+
+        printf("SINGLY LINKED LIST CREATED SUCCESSFULLY\n");
+    }
+}
+
+void insertnode(int data)
+{
+	struct node *p,*q=start;
+	p=(struct node*) malloc(sizeof(struct node));
+		p->data=data;
+	while(q->data!=7)
+	
+	{
+	q=q->next;
+
+		if(q->data==7)
+		{
+		
+			p->next=q->next;
+		q->next=p;
+		}
+	}
+	
+		
+		
+	}
+
+void displayList()
+{
+    struct node *temp;
+    if(start == NULL)
+    {
+        printf("List is empty.");
+    }
+    else
+    {
+        temp = start;
+        while(temp != NULL)
+        {
+            printf("Data = %d\n", temp->data);
+            temp = temp->next;                 
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
