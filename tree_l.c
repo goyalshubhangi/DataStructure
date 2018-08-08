2edc42bd37e2ec4abb4a79da781ff2e8071fb4a3
goyalshubhangi <36481884+goyalshubhangi@users.noreply.github.com>
2018-08-08 18:26:35 +0530

    Update tree_l.c

--- tree_l.c
+++ tree_l.c
@@ -6,7 +6,43 @@ typedef struct TREE
 	struct TREE*left;
 	struct TREE*right;
 }TREE;
-int numberOfLeaf(TREE* root)
+int number_leaf(TREE*root);
+int main()
+{
+	int data,depth;
+	TREE*tree=NULL;
+	TREE*InsertTree(int data,TREE*p);
+	TREE*PrintTreeTriangle(TREE*tree,int level);
+
+	while(1)
+	{
+		printf("\nKey to insert|");
+		scanf("%d", &data);
+		if (data==0)
+			  break;
+		tree =InsertTree(data,tree);
+		printf("\n Tree Display;\n");
+		PrintTreeTriangle(tree,1);
+		depth=0;
+	}
+printf("%d",number_leaf(tree));
+	return(0);
+}
+TREE *InsertTree(int data,TREE *p) {
+	if(!p) {
+		p=(TREE*)malloc(sizeof(TREE));
+		p->data=data;
+		p->left=NULL;
+		p->right=NULL;
+		return(p);
+	}
+	if(data < p->data)
+		p->left=InsertTree(data,p->left); else
+		if(data > p->data)
+		p->right=InsertTree(data,p->right);
+	return(p);
+}
+int number_leaf(TREE*root)
 {
 	if(root==NULL)
 		return 0;
@@ -15,23 +51,21 @@ int numberOfLeaf(TREE* root)
 		if(root->left==NULL&&root->right==NULL)
 			return 1;	
 
-		int xl= numberOfLeaf(root->left);
-		int xr= numberOfLeaf(root->right);
+		int xl= number_leaf(root->left);
+		int xr= number_leaf(root->right);
 
 		return (xl+xr);
 	}
 }
-int main()
-{
-	TREE root[]={
-							{'x',root+1,root+4},
-							{'y',root+2,root+3},
-							{'z',NULL,NULL},
-							{'a',NULL,NULL},
-							{'b',root+5,root+6},
-							{'c',NULL,NULL},
-							{'d',NULL,NULL}	};
-
-	printf("%d",numberOfLeaf(root));
-	return 0;
+TREE *PrintTreeTriangle(TREE *tree, int level) {
+	int i;
+	if(tree) {
+		PrintTreeTriangle(tree->right,level+1);
+		printf("\n\t");
+		for (i=0;i<level;i++)
+				printf("    ");
+		printf("%d",tree->data);
+		PrintTreeTriangle(tree->left,level+1);
+	}
+	return(NULL);
 }
